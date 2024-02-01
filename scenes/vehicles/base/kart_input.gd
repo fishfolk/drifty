0b64b508f21c20e7d4283bdbaead1cfe2ce19238
Natan Maia <natanmaiaplays@outlook.com>
2024-02-01 20:48:35 -0300

    very very basic placeholder movement

---
+++ scenes/vehicles/base/kart_input.gd
@@ -0,0 +1,32 @@
+extends Node
+class_name KartInput
+## Base input handler for controlling karts.
+## [br]
+## In the future, this class spans subclasses for AI and player controlled karts;
+## For now, it takes input from the global Input system.
+
+## This variable can turn on or off the handler
+@export var enabled := true
+
+var throttle: float = 0.0
+var brakes: float = 0.0
+
+## ranging from -1.0 for left to 1.0 for right.
+var steering: float = 0.0 
+
+
+## Public function to gather input. It's best called every physics frame.
+func query_input() -> void:
+	if not enabled:
+		throttle = 0
+		brakes = 0
+		steering = 0
+		return
+	_update_input()
+
+
+## Private function. This is the function to override in subclasses.
+func _update_input() -> void:
+	throttle = Input.get_action_strength("ui_up")
+	brakes = Input.get_action_strength("ui_down")
+	steering = Input.get_axis("ui_left", "ui_right")
--- scenes/vehicles/base/kinematic_kart.gd
+++ scenes/vehicles/base/kinematic_kart.gd
@@ -1,7 +1,15 @@
 extends CharacterBody3D
 
 
-const SPEED = 5.0
+@export var input: KartInput = null
+
+
+#const SPEED = 5.0
+const MAX_SPEED = 30.0 # meters per second
+const ACCEL = 4.0 # (meters per second) per second 
+
+const STEER_SPEED = 4.0 # radians per second
+
 const JUMP_VELOCITY = 4.5
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
@@ -10,29 +18,61 @@ var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 
 
+## Returns the speed along strictly the forward / backward axis of the vehicle.
+## Sideways movement and falling movement is ignored.
 func get_speed() -> float :
 	return velocity.dot(-global_transform.basis.z)
 
 
+func set_input(new_input: KartInput) -> void:
+	if input:
+		remove_child(input)
+		input.queue_free()
+	
+	input = new_input
+	add_child(new_input)
+
+
+
+
 
 func _physics_process(delta):
+	# natural deceleration
+	#velocity *= 0.95
+	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y -= gravity * delta
-
+	
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
+	
+	#print(velocity.y)
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
-
+	#var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	#var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	#if direction:
+		#velocity.x = direction.x * SPEED
+		#velocity.z = direction.z * SPEED
+	#else:
+		#velocity.x = move_toward(velocity.x, 0, SPEED)
+		#velocity.z = move_toward(velocity.z, 0, SPEED)
+	
+	if input:
+		input.query_input()
+		_update_drive(delta)
+		_update_steer(delta)
+	
 	move_and_slide()
+
+func _update_drive(delta) -> void:
+	var engine_accel : float = 0
+	if get_speed() < MAX_SPEED:
+		engine_accel = input.throttle * ACCEL * delta
+	velocity += -global_transform.basis.z * engine_accel
+
+func _update_steer(delta) -> void:
+	var steer = input.steering * STEER_SPEED * delta
+	global_rotate(-global_transform.basis.y, steer)
