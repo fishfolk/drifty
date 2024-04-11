776e8a2e5f1d7f409bba1ba769382e69eb88fed8
Natan Maia <natanmaiaplays@outlook.com>
2024-04-10 21:31:03 -0300

    Update race_manager.gd

--- scripts/autoloads/race_manager.gd
+++ scripts/autoloads/race_manager.gd
@@ -2,8 +2,49 @@ extends Node
 
 
 
+signal race_begun
+signal race_finished
+
+signal intro_flydown_started
+signal intro_flydown_finished
+
+## use current_race.lap_count for max laps instead.
+signal car_completed_lap(car, new_lap)#, max_laps)
+
+
+
+
+
+
+
+## Dict with data of everything needed to setup a race scene.
+## Includes race type, map, laps, opponents.
+var current_race = null
+
+
+## Countdown will only start after all these
+## steps have been checked.
+var race_setup_checks = {
+	"checkpoints": false,
+	"cameras": false,
+	"starting_grid": false,
+}
+
+
 ## AI Drivers will refer to this.
-## Ideally, there should be a system for multiple paths.
+## Ideally, there should be a custom system for multiple paths.
 var race_path : Path3D
 ## Homing items will refer to this.
 var item_path : Path3D
+
+
+## racers, stored by position order, then id order
+## in the future, make racer and kart different objects
+var racers : Array[SimpleRaycastCar] = []
+
+
+func setup_race(race):
+	current_race = race
+	for value in race_setup_checks.values():
+		value = false
+	#get_tree().change_scene_to_file(race.track_file)
